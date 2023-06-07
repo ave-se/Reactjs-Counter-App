9ccdfac6176af77c792c384439e03fc7247ed8a9
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-06-07 19:23:14 +0200

    Bootstrap

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Counter App</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
